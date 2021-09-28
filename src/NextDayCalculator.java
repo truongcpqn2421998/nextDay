3ebab49902f508bdc5b1c2c8ad645718bfeb6917
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-09-28 11:47:13 +0700

    passTest2

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -3,6 +3,12 @@ public class NextDayCalculator {
     public static final String CONCAT = "/";
 
     public static String nextDay(int day, int month, int year){
-        return ++day+ CONCAT +month+CONCAT+year;
+        if(day==31){
+            day=1;
+            month++;
+        }else {
+            day++;
+        }
+        return day+ CONCAT +month+CONCAT+year;
     }
 }

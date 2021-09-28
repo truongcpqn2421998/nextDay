9da633a48bbc6a48138ce8bf9d83f50c413195c1
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-09-28 13:12:10 +0700

    addTest4vs5

--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -27,10 +27,30 @@ class NextDayCalculatorTest {
     @org.junit.jupiter.api.Test
     @DisplayName("case 30/4/2018")
     void tesDay30Month4Year2018() {
-        int dayTest=3;
+        int dayTest=30;
         int monthTest=4;
         int yearTest=2018;
         String expected="1/5/2018";
+        String result=NextDayCalculator.nextDay(dayTest,monthTest,yearTest);
+        assertEquals(expected,result);
+    }
+    @org.junit.jupiter.api.Test
+    @DisplayName("case 28/2/2018")
+    void tesDay28Month2Year2018() {
+        int dayTest=28;
+        int monthTest=2;
+        int yearTest=2018;
+        String expected="1/3/2018";
+        String result=NextDayCalculator.nextDay(dayTest,monthTest,yearTest);
+        assertEquals(expected,result);
+    }
+    @org.junit.jupiter.api.Test
+    @DisplayName("case 29/2/2020")
+    void tesDay29Month2Year2020() {
+        int dayTest=29;
+        int monthTest=2;
+        int yearTest=2020;
+        String expected="1/3/2020";
         String result=NextDayCalculator.nextDay(dayTest,monthTest,yearTest);
         assertEquals(expected,result);
     }
